c345bbc32fb4a33629f39b504ac34819d9466b1d
Ancelmo Luiz <ancelmo.luiz.theos@hotmail.com>
2026-03-07 19:21:15 -0300

    Atividades para próxima aula

--- TesteDoDominio/Program.cs
+++ TesteDoDominio/Program.cs
@@ -9,6 +9,8 @@ var listaProdutos = new List<Produto>();
 listaProdutos.Add(new Produto(1, "iPhone", "telefone apple", 2000m, TipoProdutoEnum.Telefonia, 20));
 listaProdutos.Add(new Produto(2, "Samsung S20", "telefone samsung", 1500m, TipoProdutoEnum.Telefonia, 10));
 
+Console.WriteLine();
+
 foreach (var item in listaProdutos)
 {
     Console.WriteLine($"Id: {item.Id} - Nome: {item.Nome} - Descrição: {item.Descricao} - Preço: {item.Preco} - Tipo Produto: {item.TipoProduto} - Quantidade Estoque: {item.QuantidadeEstoque}");
@@ -18,14 +20,18 @@ Console.WriteLine();
 
 Console.WriteLine("Novo Cliente");
 var cliente = new Cliente(1, "João Silva", "123.456.789-00", new DateTime(1990, 5, 20), "teste@teste.com", "69 9999-8888");
+Console.WriteLine($"Nome: {cliente.Nome} ");
+
+Console.WriteLine();
 
 var pedido = new Pedido(cliente.Id, StatusPedidoEnum.Aberto);
 
 pedido.AdicionarItem(listaProdutos[0], 2);
 pedido.AdicionarItem(listaProdutos[1], 1);
 
-
 Console.WriteLine($"Pedido do cliente: {cliente.Nome} - Status: {pedido.Status} - Data Cadastro: {pedido.DataCadastro}");
+Console.WriteLine();
+
 Console.WriteLine($"Itens do Pedido:");
 foreach (var item in pedido.Itens)
 {
@@ -35,3 +41,8 @@ foreach (var item in pedido.Itens)
 Console.WriteLine();
 Console.WriteLine($"Valor Total do Pedido: R$ {pedido.ValorTotal} ");
 
+//FECHAR O PEDIDO
+
+//PAGAR O PEDIDO
+
+//BAIXAR O ESTOQUE DOS PRODUTOS DO PEDIDO
