4782d8ca3bfca426afef86e99fb578b961e1d3b3
Ancelmo Luiz <ancelmo.luiz.theos@hotmail.com>
2026-04-04 18:49:57 -0300

    Aula 04/04/2026 início da camada de infraestrura

---
+++ GuimaraesStore.Infra/Contexto/GuimaraesStoreContext.cs
@@ -0,0 +1,23 @@
+﻿using GuimaraesStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuimaraesStore.Infra.Contexto
+{
+    public class GuimaraesStoreContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedido { get; set; }
+
+        public GuimaraesStoreContext(DbContextOptions<GuimaraesStoreContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
